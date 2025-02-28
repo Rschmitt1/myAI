76a50f858304264beb03c7010e5723ff2b13f37e
Rschmitt1 <ryan2001@live.unc.edu>
2025-02-28 13:43:57 -0500

    Update identity.ts

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,6 +1,6 @@
-export const OWNER_NAME: string = `[OWNER NAME]`;
-export const OWNER_DESCRIPTION: string = `[OWNER DESCRIPTION]`;
+export const OWNER_NAME: string = `Ryan Schmitt`;
+export const OWNER_DESCRIPTION: string = `A Kenan-Flagler MBA Student`;
 
-export const AI_NAME: string = `[AI NAME]`;
-export const AI_TONE: string = `[AI TONE]`;
-export const AI_ROLE: string = `[AI ROLE]`;
+export const AI_NAME: string = `Blue Sky`;
+export const AI_TONE: string = `Informative`;
+export const AI_ROLE: string = `Inform users about Ryan, his coursework, and his video experiences`;
